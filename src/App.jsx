d8afdc4f447fe476e7c0ad3a04f32373ab574190
jasnmar <jasnmar@gmail.com>
2024-04-19 06:46:55 -0400

    Passing the object

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,11 @@ import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import data from './data'
 
-
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
-
 function App() {
   const cards = data.map((card) => {
     return <Card 
       key={card.id}
-      img={card.coverImg} 
-      rating={card.rating} 
-      reviewCount={card.stats.reviewCount} 
-      location={card.location} 
-      title={card.title} 
-      price={card.price} 
-      openSpots={card.openSpots}
+      item={card}
       /> 
   })
   return (
@@ -47,14 +27,3 @@ function App() {
 
 export default App
 
-
-/*
-<Card 
-        img="katie-zaferes.png" 
-        rating="5.0" 
-        reviewCount={6} 
-        country="USA" 
-        title="Life Lessons with Katie Zaferes" 
-        price={136} 
-      />
-*/
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,19 +3,17 @@ import "./Card.css"
 import star from "../../assets/star.png"
 
 
-function Card(props) {
-    const imgSrc = "/src/assets/" + props.img
+function Card(item) {
+    const props = item.item
+    const imgSrc = "/src/assets/" + props.coverImg
     let badgeText = ""
     if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
     } else if (props.location=== "Online") {
         badgeText = "ONLINE"
     }
-
         /*
-    Challenge:
-    1. Display the correct text in the badge based on the logic above
-    2. Only display the badge if badgeText has a value
+    Challenge: Fix our component! 😱
     */
     return (
         <div className="card">
@@ -23,9 +21,9 @@ function Card(props) {
             <img className="card--image" src={imgSrc}></img>
             <div className="card--rating">
                 <img className="card--star" src={star}></img> 
-                <p className="card--ratingtext">{props.rating} 
+                <p className="card--ratingtext">{props.stats.rating} 
                     <span className="card--ratingtext-light">
-                    ({props.reviewCount}) &#x2022; {props.location}</span>
+                    ({props.stats.reviewCount}) &#x2022; {props.location}</span>
                 </p>
             </div>
             <h2 className="card--title">{props.title}</h2>
