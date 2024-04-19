a72c0f3b99fe1dea6df036f0669012c713d4828e
jasnmar <jasnmar@gmail.com>
2024-04-19 05:23:09 -0400

    Passing Props

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import Card from './components/Card/Card'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
-function App() {
- 
 
-  return (
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
+function App() {
+   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <Card img="katie-zaferes.png" rating="5.0" reviewCount={6} country="USA" title="Life Lessons with Katie Zaferes" price={136} />
       <h1>app</h1>
         
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,23 @@
 import "./Card.css"
-import katie from "../../assets/katie-zaferes.png"
+//import katie from "../../assets/katie-zaferes.png"
 import star from "../../assets/star.png"
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
 
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-function Card() {
+function Card(props) {
+    console.log(props)
+    const imgSrc = "/src/assets/" + props.img
     return (
         <div className="card">
-            <img className="card--image" src={katie}></img>
+            <img className="card--image" src={imgSrc}></img>
             <div className="card--rating">
                 <img className="card--star" src={star}></img> 
-                <p className="card--ratingtext">5.0 <span className="card--ratingtext-light">(6) &#x2022; USA</span></p>
+                <p className="card--ratingtext">{props.rating} <span className="card--ratingtext-light">({props.reviewCount}) &#x2022; {props.country}</span></p>
             </div>
-            <p className="card--title">Life lessons with Katie Zaferes</p>
-            <p className="card--price"><span className="card--price--bold">From $136</span> / person</p>
+            <h2 className="card--title">{props.title}</h2>
+            <p className="card--price"><span className="card--price--bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
 
 export default Card
+
